764f8d0e73fa0b58ab0314476fb1fe8561c17061
natenhagen <natepappenhagen@gmail.com>
2018-06-23 11:39:36 -0600

    chapter 6 complete - left the shire

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -88,7 +88,7 @@ const makeHobbits = () => {
     const $ul = $('<ul>').appendTo($('#The-Shire'));
 
     for (let i = 0; i < hobbits.length; i++){
-    $('<li>').addClass('hobbit').text(hobbits[i]).appendTo($ul);
+    $('<li>').addClass('hobbits').text(hobbits[i]).appendTo($ul);
     }
 
 
@@ -161,9 +161,18 @@ const makeBuddies = () => {
 // ============
 const leaveTheShire = () => {
 
-  // 1. grab the hobbits (the ul in which they reside) and move them to Rivendell
+  // 1. grab the hobbits (the ul in which they reside)
+  //   and move them to Rivendell
 
-  // hint: the hobbits ul is a childNode of The-Shire-- there is way to get a list of childNodes
+
+  const $hobbitsLi = $('.hobbits').detach();
+  $hobbitsLi.appendTo('#Rivendell');
+
+
+  // hint: the hobbits ul is a childNode of The-Shire--
+  //  there is way to get a list of childNodes
+
+
 
 };
 
